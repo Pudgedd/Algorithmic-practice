5c4c657254826047d2408c3294a71c769f6f6342
kendall <190708208@qq.com>
2018-10-23 22:41:13 +0800

    copy random list node

---
+++ src/common/RandomListNode.java
@@ -0,0 +1,40 @@
+package common;
+
+/**
+ * @description:
+ * @author: kendall
+ * @since: 2018/10/23
+ */
+public class RandomListNode {
+    private int label;
+    private RandomListNode next = null;
+    private RandomListNode random = null;
+
+    public RandomListNode(int label) {
+        this.label = label;
+    }
+
+    public int getLabel() {
+        return label;
+    }
+
+    public void setLabel(int label) {
+        this.label = label;
+    }
+
+    public RandomListNode getNext() {
+        return next;
+    }
+
+    public void setNext(RandomListNode next) {
+        this.next = next;
+    }
+
+    public RandomListNode getRandom() {
+        return random;
+    }
+
+    public void setRandom(RandomListNode random) {
+        this.random = random;
+    }
+}
